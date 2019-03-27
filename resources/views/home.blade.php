4a58bcafd0dee7c2d04560a8420460098aca62b1
wahid09 <wahid.ullah.cse@gmail.com>
2019-03-27 12:22:29 +0600

    User dashboard is created after registration but not email verified

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,37 @@
-@extends('layouts.app')
+@extends('user/app')
+@section('head')
+<style type="text/css" media="screen">
+    
+    header.inner-header .top-header div.left-side > ul > li > a,
+    header.inner-header .top-header .bootstrap-select.btn-group .dropdown-toggle .filter-option,
+    header.inner-header .bootstrap-select.btn-group .dropdown-toggle .caret:before {
+    color: black;
+}
+</style>
+@endsection
+@section('main-content')
+ <div class="main-content">
+            <div class="container">
+                <div class="row">
+                    
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
+                <div class="dash">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    Your Account is : {{auth()->user()->verified() ? 'Verified' : 'Not verified . Please go to your email and verify your account. Thank you for registration.'}} 
+                    <p class="lead">Your Account is : {{auth()->user()->verified() ? 'Verified' : 'Not verified . Please go to your email and verify your account. Thank you for registration.'}} </p>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+        </div>
+
 @endsection
+
+
+
+
+
+
